dea2573917541fed0679bda7f2389eaf23214634
MasterMeteor <39095894+MasterMeteor@users.noreply.github.com>
2019-02-02 15:25:12 +0800

    Math Print
    数学公示的输入和表达

---
+++ mathprint.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+#Python支持五种基本数据类型：
+# Numbers
+# String List
+# Tuple元组（组（tuple）是关系数据库中的基本概念，关系是一张表，表中的每行（即数据库中的每条记录）就是一个元组，每列就
+# 是一个属性。 在二维表里，元组也称为行。
+# Dictionary
+# ）
+#------------------
+#1
+print('\n#1')#'\n'表示回车空一行
+x=10
+y=22
+z=35
+print('x,y,z,',x,y,z)
+
+#2
+print('\n#2')
+a=x+y;print('a=x+y,',a)
+b=x-y;print('b=x-y,',b)
+c=z-x*y;print('c=z-x*y,',c)
+
+#3
+print('\n#3')
+a=z/x;print('a=z/x,',a)
+b=z//x;print('b=z//x,',b)
+c=z%x;print('c=z%x,',c)
+
+#4
+print('\n#4')
+a=x**2;print('a=x**2,',a)
+b=x**3;print('b=x**3,',b)
